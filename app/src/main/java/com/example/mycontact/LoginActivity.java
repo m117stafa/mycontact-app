ec961a892c50cecd9c8328c18330d46d5931ba76
m117stafa <mastaphaaz2001@gmail.com>
2023-03-19 05:45:13 +0100

    login & signup is done

--- app/src/main/java/com/example/mycontact/LoginActivity.java
+++ app/src/main/java/com/example/mycontact/LoginActivity.java
@@ -1,15 +1,28 @@
 package com.example.mycontact;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.List;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -21,23 +34,79 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
         editTextEmailLogin = findViewById(R.id.email);
         editTextMotpassLogin = findViewById(R.id.motpass);
         loginButton = findViewById(R.id.btn_se_connecter);
         textInscription = findViewById(R.id.text_inscri);
 
+
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(LoginActivity.this,"login 5snd",Toast.LENGTH_SHORT).show();
-            }
-        });
 
-        textInscription.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
-                startActivity(intent);
+                String emailLogin, motpassLogin;
+                emailLogin = String.valueOf(editTextEmailLogin.getText());
+                motpassLogin = String.valueOf(editTextMotpassLogin.getText());
+
+                if (TextUtils.isEmpty(emailLogin) || TextUtils.isEmpty(motpassLogin)) {
+                    Toast.makeText(LoginActivity.this, "Remplir tous les cases", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                final FirebaseFirestore db = FirebaseFirestore.getInstance();
+                Log.d(TAG, "onClick: ----- 9bal db collection");
+                Log.d(TAG, "onClick: -------" + emailLogin + "  " + motpassLogin);
+                db.collection("users").whereEqualTo("emailUser", emailLogin)
+                        .get()
+                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+
+                                if (task.isSuccessful()) {
+
+                                    if (task.getResult().isEmpty()) {
+                                        Log.d(TAG, "onComplete: info ghaltin hhh ");
+                                        Toast.makeText(LoginActivity.this, "les informations sont incorrect", Toast.LENGTH_SHORT).show();
+                                        editTextEmailLogin.requestFocus();
+                                        return;
+                                    }
+                                    DocumentSnapshot doc = task.getResult().getDocuments().get(0);
+                                    String emailFromDb = (String) doc.get("emailUser");
+                                    String motpassFromDb = (String) doc.get("motPass");
+                                    Log.d(TAG, "onComplete: " + emailFromDb + " " + motpassFromDb);
+                                    if (emailFromDb.equals(emailLogin) && motpassFromDb.equals(motpassLogin)) {
+                                        Log.d(TAG, "onComplete: sgsdgggggggggggggggggggg");
+                                        Toast.makeText(LoginActivity.this, "Vous etes connecte", Toast.LENGTH_SHORT).show();
+
+                                        Intent intent = new Intent(LoginActivity.this, ListContactActivity.class);
+                                        intent.putExtra("nom", (String) doc.get("nomUser"));
+                                        intent.putExtra("prenom", (String) doc.get("prenomUser"));
+                                        intent.putExtra("num", (String) doc.get("numUser"));
+                                        intent.putExtra("email", (String) doc.get("emailUser"));
+                                        intent.putExtra("uid", (String) doc.getId());
+                                        startActivity(intent);
+                                    } else {                                         Log.d(TAG, "onComplete: info ghaltin hhh ");
+                                    Toast.makeText(LoginActivity.this, "les informations sont incorrect", Toast.LENGTH_SHORT).show();
+                                    editTextEmailLogin.requestFocus();
+                                    return;
+                                }
+                                } else {
+                                    Toast.makeText(LoginActivity.this, "il ya un problem", Toast.LENGTH_SHORT).show();
+                                    return;
+                                }
+
+                            }
+                        });
+
+                textInscription.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                        startActivity(intent);
+                    }
+                });
+
             }
         });
     }
--- app/src/main/java/com/example/mycontact/RegisterActivity.java
+++ app/src/main/java/com/example/mycontact/RegisterActivity.java
@@ -14,7 +14,7 @@ import android.widget.Toast;
 
 import com.example.mycontact.models.User;
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
+
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
@@ -55,36 +55,41 @@ public class RegisterActivity extends AppCompatActivity {
                 prenomUser = String.valueOf(editTextPrenom.getText());
                 numUser = String.valueOf(editTextNum.getText());
 
-                FirebaseFirestore db = FirebaseFirestore.getInstance();
+                final FirebaseFirestore db = FirebaseFirestore.getInstance();
 
                 if (TextUtils.isEmpty(emailUser) || TextUtils.isEmpty(motpassUser) || TextUtils.isEmpty(nomUser) || TextUtils.isEmpty(prenomUser) || TextUtils.isEmpty(numUser)){
                     Toast.makeText(RegisterActivity.this,"Remplir tous les cases",Toast.LENGTH_SHORT).show();
                     return;
                 }
 
-                Query checkEmail = db.collection("users").whereEqualTo("emailUser",emailUser);
-
-
-
-                db.collection("users")
-                        .add(new User(emailUser,numUser,motpassUser,nomUser,prenomUser))
-                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                db.collection("users").whereEqualTo("emailUser",emailUser)
+                        .get()
+                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                             @Override
-                            public void onSuccess(DocumentReference documentReference) {
-                                Log.d("TAG","DocumentSnapshot added with ID: "+ documentReference.getId());
-                                Toast.makeText(RegisterActivity.this,"Vous etes inscrit maintenant", Toast.LENGTH_SHORT).show();
-                                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
-                                startActivity(intent);
-                            }
-                        })
-                        .addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                Log.w("TAG", "Error adding document", e);
-                                Toast.makeText(RegisterActivity.this,"Il y a un problem", Toast.LENGTH_SHORT).show();
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    if (task.getResult().isEmpty()) {
+                                        db.collection("users")
+                                                .add(new User(emailUser,numUser,motpassUser,nomUser,prenomUser))
+                                                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                                                    @Override
+                                                    public void onSuccess(DocumentReference documentReference) {
+                                                        Log.d("TAG","DocumentSnapshot added with ID: "+ documentReference.getId());
+                                                        Toast.makeText(RegisterActivity.this,"Vous etes inscrit maintenant", Toast.LENGTH_SHORT).show();
+                                                        Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                                                        startActivity(intent);
+                                                    }
+                                                });
+                                    }
+                                    else {
+                                        Toast.makeText(RegisterActivity.this,"Compte est deja exist", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                                else {
+                                    Toast.makeText(RegisterActivity.this,"il ya un problem", Toast.LENGTH_SHORT).show();
+                                }
                             }
                         });
-
 
             }
         });
@@ -103,6 +108,3 @@ public class RegisterActivity extends AppCompatActivity {
 
 
     }
-
-
-}
